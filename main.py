eb9251529bce09094f45031f2b474f05d090d797
Andrii Bobrov <aspyrin77@gmail.com>
2022-08-07 17:55:15 +0300

    fix bugs

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
 from fastapi import FastAPI, File
-# from fastapi import UploadFile
-from pydantic import BaseModel, AnyHttpUrl, ValidationError, validator
-from urllib.request import urlopen
 from fastapi.responses import StreamingResponse
 from PIL import Image
 from io import BytesIO
 
 app = FastAPI()
 
-ALLOWED_EXTENSIONS = {'svg', 'png', 'jpg', 'jpeg', 'gif'}
-ROOT_MESSAGE = {
-        "app_name": "Image converter",
-        "allowed extensions": ALLOWED_EXTENSIONS,
-        "paths": {
-            "convert_to_grayscale": "/convert-to-grayscale",
-            "convert_to_grayscale_from_url": "/convert-to-grayscale-from-url",
-        }
-    }
-
-
-class Message(BaseModel):
-    url: AnyHttpUrl
-
-    @validator('url')
-    def url_has_allowed_ext(cls, value=str):
-        link_has_extension = False
-        for ext in ALLOWED_EXTENSIONS:
-            if f'.{ext}' in value.lower():
-                link_has_extension = True
-        if not link_has_extension:
-            raise ValueError('link not look at image')
-        return value
-
 
 @app.get("/")
 async def root():
-    return ROOT_MESSAGE
+    message = {
+        "app_name": "Image converter",
+    }
+    return message
 
 
 @app.post("/convert-to-grayscale")
@@ -77,53 +53,3 @@ async def convert_to_grayscale(file: bytes = File()):
 
     else:
         return "File size 0"
-
-
-@app.post("/convert-to-grayscale-from-url")
-async def convert_to_grayscale_from_url(parameters: Message):
-    """
-    POST, function receives url, check url, get file from url, check extension,
-    convert image to grayscale and return it as image/png
-    :param url in JSON:
-    :return: edited image in initial format
-    """
-
-    # get url from parameters
-    try:
-        url = parameters.url
-    except ValidationError as e:
-        return {"Exception": e.json()}
-
-    # get file from url
-    try:
-        with urlopen(url) as url:
-            image_source = Image.open(url)
-            image_format = image_source.format
-
-    except Exception as e:
-        if e.code == 403:
-            return {"Exception": '403 HTTP Error, This Link is forbidden'}
-        else:
-            return {"Exception": e}
-
-    # convert image greyscale
-    image_edited = image_source.convert("L")
-
-    # create binary object from edited image
-    with BytesIO() as output:
-        image_edited.save(output, format=image_format)
-        img_bytes_object = output.getvalue()
-
-    img_io_object = BytesIO(img_bytes_object)
-    bytes_count = img_bytes_object.__sizeof__()
-
-    # create response
-    response: StreamingResponse = StreamingResponse(img_io_object, media_type=f"image/{image_format}")
-    response.headers['content-type'] = f"image/{image_format}"
-    response.headers['content-length'] = str(bytes_count)
-    response.headers['Transfer-Encoding'] = "chunked"
-
-    # send image_edited as attachment for download
-    # response.headers["Content-Disposition"] = f"attachment; filename=edited_image.{image_format}"
-
-    return response
--- test_main.py
+++ test_main.py
@@ -15,13 +15,3 @@ def test_convert_to_grayscale():
     response = client.post(url, files={"file": ("filename", open(filename, "rb"), "image/jpeg")})
     assert response.status_code == 200
     assert int(response.headers["content-length"]) > 0
-
-
-def test_convert_to_grayscale_from_url():
-    data = {
-        "url": "https://planbphoto.com/wp-content/uploads/Serze.jpg"
-    }
-    url = '/convert-to-grayscale-from-url'
-    response = client.post(url, json=data)
-    assert response.status_code == 200
-    assert int(response.headers["content-length"]) > 0
